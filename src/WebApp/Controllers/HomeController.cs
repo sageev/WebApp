d81d922c8eaa4ca176a6ab5429b0e23011fefb3a
Stan <sageev@hotmail.com>
2016-11-28 17:42:40 -0600

    Changed About view

--- src/WebApp/Controllers/HomeController.cs
+++ src/WebApp/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
 
 namespace WebApp.Controllers
 {
@@ -15,7 +12,7 @@ namespace WebApp.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "test"; // System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            ViewData["Message"] = RuntimeInformation.OSDescription;
 
             return View();
         }
