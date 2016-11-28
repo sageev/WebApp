87666a74840778defa7438f847279fe051aab6f7
sageev <sageev@users.noreply.github.com>
2016-11-28 17:12:50 -0600

    Update HomeController.cs

--- src/WebApp/Controllers/HomeController.cs
+++ src/WebApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApp.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
 
             return View();
         }
